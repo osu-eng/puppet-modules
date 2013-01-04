a224b0f1622ec370c67d9e3aad6d0378c8d25c08
jasonlttl <little.129@osu.edu>
2013-01-04 08:25:07 -0500

    removed file fixed repo manifest

--- shibboleth_server/manifests/repos.pp
+++ shibboleth_server/manifests/repos.pp
@@ -1,21 +1,10 @@
 class shibboleth_server::repos {
 
-  $gpgkey = '/etc/pki/rpm-gpg/RPM-GPG-KEY-shibboleth'
-
   yumrepo { 'security_shibboleth':
     descr    => 'Shibboleth (OpenSuse Security) - $basearch',
-    baseurl  => 'http://download.opensuse.org/repositories/security:/shibboleth/RHEL_6/$basearch',
-    gpgkey   => "file:///${gpgkey}",
+    baseurl  => 'http://download.opensuse.org/repositories/security:/shibboleth/RHEL_6/',
+    gpgkey   => 'http://download.opensuse.org/repositories/security:/shibboleth/RHEL_6/repodata/repomd.xml.key',
     enabled  => 1,
     gpgcheck => 1,
-    require  => File[$gpgkey],
-  }
-
-  file { $gpgkey:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/shibboleth_server/RPM-GPG-KEY-shibboleth',
   }
 }
