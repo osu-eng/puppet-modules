2a6a0e65971c84d46d293b328bf8b8d2c800ad69
Corey Hinshaw <hinshaw.25@osu.edu>
2013-03-04 14:41:38 -0500

    Added piranha password configuration

--- piranha/manifests/init.pp
+++ piranha/manifests/init.pp
@@ -1,5 +1,7 @@
 class piranha(
-  $repo_url = $piranha::params::repo_url
+  $repo_url = $piranha::params::repo_url,
+  $virtual_ip = $piranha::params::virtual_ip
+  $gui_password = $piranha::params::gui_password
 ) inherits piranha::params {
   $gpgkey = '/etc/pki/rpm-gpg/RPM-GPG-KEY-rhel-local-lb'
 
@@ -31,6 +33,7 @@ class piranha(
     ensure     => running,
     enable     => true,
     require    => Package['piranha'],
+    subscribe  => File['/etc/sysconfig/ha/conf/piranha.passwd'],
   }
   service { 'pulse':
     ensure     => running,
@@ -38,10 +41,27 @@ class piranha(
     require    => Package['piranha'],
   }
 
+  file { '/etc/sysconfig/ha/conf/piranha.passwd':
+    ensure  => present,
+    owner   => 'piranha',
+    group   => 'piranha',
+    mode    => '0600',
+    require => Package['piranha'],
+    content => template('piranha/piranha.password.erb'),
+  }
+
   if defined('firewall::rule') {
     firewall::rule { 'allow-piranha-gui':
       weight => '320',
       rule   => '-A INPUT -p tcp -m state --state NEW -m tcp --dport 3636 -j ACCEPT',
     }
+    define piranha::markvip {
+      firewall::rule { "bundle-http-https-${title}":
+        weight => '110',
+        table  => 'mangle',
+        rule   => "-A PREROUTING -p tcp -d ${title}/32 -m multiport --dports 80,443 -j MARK --set-mark 80",
+      }
+    }
+    piranha::markvip { $virtual_ip: }
   }
 }
--- piranha/manifests/params.pp
+++ piranha/manifests/params.pp
@@ -1,4 +1,5 @@
 class piranha::params {
   $repo_url = "http://example.com/repo"
   $virtual_ip = [ '192.168.1.50' ]
+  $gui_password = ".x0MxPyN/65DU"
 }
