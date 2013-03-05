9a5df195c14d0cff067dfe49f91e71613e782e47
Corey Hinshaw <hinshaw.25@osu.edu>
2013-03-05 16:46:51 -0500

    Removed extraneous exec command from piranha::add_vip

--- piranha/manifests/add_vip.pp
+++ piranha/manifests/add_vip.pp
@@ -1,12 +1,6 @@
 define piranha::add_vip ($ip = $title, $netmask, $interface) {
   include rclocal
 
-  exec { "piranha-add-${ip}":
-    provider => shell,
-    command  => "/sbin/ip addr add ${ip} dev ${interface}",
-    unless   => "/sbin/ip addr | /bin/grep ${ip} 2>/dev/null",
-  }
-
   rclocal::script { "piranha-persist-${ip}":
     priority => '10',
     content  => "ip addr add ${ip} dev ${interface} \n",
