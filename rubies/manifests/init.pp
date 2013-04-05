4d53d4647efaddecff362a9ee5fe317d7c7b5367
Corey Hinshaw <hinshaw.25@osu.edu>
2013-04-05 12:46:33 -0400

    Added dependency packages to rubies module

--- rubies/manifests/init.pp
+++ rubies/manifests/init.pp
@@ -3,8 +3,22 @@ class rubies(
   $rvm_users = $rubies::params::rvm_users
 ) inherits rubies::params {
 
+  include epel
   include rvm
 
-  rubies::ruby { $versions: }
-  rvm::system_user { $rvm_users: }
+  $packages = [ 'libyaml-devel', 'libffi-devel', 'libtool', 'bison' ]
+
+  package { $packages:
+    ensure => present,
+  }
+
+  rubies::ruby { $versions:
+    ensure  => present,
+    require => Package[$packages],
+  }
+
+  rvm::system_user { $rvm_users:
+    ensure  => present,
+    require => Package[$packages],
+  }
 }
