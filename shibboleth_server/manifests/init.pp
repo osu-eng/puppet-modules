7699ee3a3f77414dbae3839640fefba53f5233e8
little.129@osu.edu <little.129@osu.edu>
2013-04-11 17:51:50 -0400

    made key readable to shibd

--- shibboleth_server/manifests/init.pp
+++ shibboleth_server/manifests/init.pp
@@ -1,7 +1,7 @@
 class shibboleth_server (
   $entity = $shibboleth_server::params::entity,
   $session_lifetime = $shibboleth_server::params::session_lifetime,
-  $session_timeout = $shibboleth_server::params::session_timeout, 
+  $session_timeout = $shibboleth_server::params::session_timeout,
   $session_relay_state = $shibboleth_server::params::session_relay_state,
   $session_check_address = hiera('shibboleth_server::session_check_address', $shibboleth_server::params::session_check_address),
   $session_handler_ssl = hiera('shibboleth_server::session_handler_ssl', $shibboleth_server::params::session_handler_ssl),
@@ -11,7 +11,7 @@ class shibboleth_server (
   $cert_source = $shibboleth_server::params::cert_source,
 ) inherits shibboleth_server::params {
 
-  # Important Files 
+  # Important Files
   $shibboleth2      = '/etc/shibboleth/shibboleth2.xml'
   $attribute_map    = '/etc/shibboleth/attribute-map.xml'
   $attribute_policy = '/etc/shibboleth/OSU-attribute-policy.xml'
@@ -26,7 +26,7 @@ class shibboleth_server (
     ensure => present,
   }
 
-  # Enable Service 
+  # Enable Service
   service { 'shibd':
     ensure     => 'running',
     enable     => true,
@@ -41,14 +41,14 @@ class shibboleth_server (
     group   => 'root',
     mode    => '0644',
     content => template('shibboleth_server/shibboleth2.xml.erb'),
-  }    
+  }
   file { $attribute_map:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     source  => 'puppet:///modules/shibboleth_server/attribute-map.xml',
-  }  
+  }
   file { $attribute_policy:
     ensure  => present,
     owner   => 'root',
@@ -62,19 +62,19 @@ class shibboleth_server (
     group   => 'root',
     mode    => '0644',
     source  => 'puppet:///modules/shibboleth_server/OSU-metadata.pem',
-  }    
+  }
   file { $session_error:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template('shibboleth_server/sessionError.html.erb'),
-  }  
+  }
   file { $key:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
-    mode    => '0600',
+    mode    => '0644',
     source  => $key_source,
   }
   file { $cert:
