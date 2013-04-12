292d981474afacdb2779fbec399945c27305dd57
little.129@osu.edu <little.129@osu.edu>
2013-04-12 16:54:21 -0400

    typo in selinux comm

--- aegir/files/selinux.sh
+++ aegir/files/selinux.sh
@@ -1,7 +1,7 @@
 chcon -R --type=httpd_sys_content_t /var/aegir
 chcon -R --type=ssh_home_t  /var/aegir/.ssh
 chcon -R --type=httpd_config_t /var/aegir/config
-chcon -v --type=ssh_home_t /var/aegir/.ssh/authorized_keys
+chcon -R --type=ssh_home_t /var/aegir/.ssh/authorized_keys
 semanage fcontext -a -t httpd_sys_content_t /var/aegir
 semanage fcontext -a -t ssh_home_t  /var/aegir/.ssh
 semanage fcontext -a -t httpd_config_t /var/aegir/config
