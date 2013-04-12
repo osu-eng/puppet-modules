3aaed95c08d1f9ce1e35fa503d8e312f5b1031fa
little.129@osu.edu <little.129@osu.edu>
2013-04-12 16:50:23 -0400

    ssh & aegir & selinux take 2

--- aegir/files/selinux.sh
+++ aegir/files/selinux.sh
@@ -1,8 +1,8 @@
 chcon -R --type=httpd_sys_content_t /var/aegir
-chcon -R --type=var_t /var/aegir/.ssh
+chcon -R --type=ssh_home_t  /var/aegir/.ssh
 chcon -R --type=httpd_config_t /var/aegir/config
 chcon -v --type=ssh_home_t /var/aegir/.ssh/authorized_keys
 semanage fcontext -a -t httpd_sys_content_t /var/aegir
-semanage fcontext -a -t var_t /var/aegir/.ssh
+semanage fcontext -a -t ssh_home_t  /var/aegir/.ssh
 semanage fcontext -a -t httpd_config_t /var/aegir/config
 semanage fcontext -a -t ssh_home_t /var/aegir/.ssh/authorized_keys
