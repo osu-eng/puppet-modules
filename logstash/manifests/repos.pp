0e50f73e8896aa080ccedc9e5075ce26a85cbe70
little.129@osu.edu <little.129@osu.edu>
2013-10-11 16:42:59 -0400

    corrected epel repo

--- logstash/manifests/repos.pp
+++ logstash/manifests/repos.pp
@@ -1,6 +1,8 @@
+class logstash::repos {
   yumrepo { 'epel':
     mirrorlist     => "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-${::os_maj_version}&arch=${::architecture}",
     failovermethod => 'priority',
     enabled  => 1,
     gpgcheck => 0,
   }
+}
