276d0f7f562fd47a25e9ea62f29b797275eafccb
Brian Kildow <bkildow@gmail.com>
2014-07-10 10:02:33 -0400

    Added symlink

--- kibana/manifests/init.pp
+++ kibana/manifests/init.pp
@@ -7,7 +7,12 @@ class kibana (
     url    => 'https://download.elasticsearch.org/kibana/kibana/kibana-3.1.0.tar.gz',
     target => '/var/www',
     checksum => false
-  } 
+  }
+
+  file { '/var/www/kibana':
+    ensure => 'link',
+    target => '/var/www/kibana-3.1.0',
+  }
 
   file { '/var/www/kibana/config.js':
     ensure  => present,
