8c66965672efd9c7a5f51c899ab8241521e33504
Brian Kildow <bkildow@gmail.com>
2014-07-14 15:07:00 -0400

    Refactored logstashforwarder settings into a module

---
+++ log_client/manifests/init.pp
@@ -0,0 +1,40 @@
+class log_client {
+
+  file { '/etc/logstashforwarder/ssl/public.cert':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///private/etc/httpd/conf/public.cert',
+  } 
+
+  file { '/etc/logstashforwarder/ssl/ca-chain.cert':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///private/etc/httpd/conf/ca-chain.cert',
+  } 
+
+  file { '/etc/logstashforwarder/ssl/private.key':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///private/etc/httpd/conf/private.key',
+  }
+
+  class { 'logstashforwarder':
+    manage_repo  => true,
+    servers  => [ 'logstash.web.engineering.osu.edu:4545' ],
+    ssl_key  => '/etc/logstashforwarder/ssl/private.key',
+    ssl_ca   => '/etc/logstashforwarder/ssl/ca-chain.cert',
+    ssl_cert => '/etc/logstashforwarder/ssl/public.cert'
+  }
+
+  logstashforwarder::file { 'messages':
+    paths  => [ '/var/log/messages', '/var/log/secure' ],
+    fields => { 'type' => 'syslog' }
+  }
+
+}
---
+++ log_client/manifests/web.pp
@@ -0,0 +1,16 @@
+class log_client::web {
+  logstashforwarder::file { 'apache-access':
+    paths  => [ '/var/log/httpd/access_log' ],
+    fields => { 'type' => 'apache-access' }
+  }
+
+  logstashforwarder::file { 'apache-access-ssl':
+    paths  => [ '/var/log/httpd/ssl_access_log' ],
+    fields => { 'type' => 'apache-access-ssl' }
+  }
+
+  logstashforwarder::file { 'apache-error':
+    paths  => [ '/var/log/httpd/error_log', '/var/log/httpd/ssl_error_log'],
+    fields => { 'type' => 'apache-error' }
+  }	
+}
