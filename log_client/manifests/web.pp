59f078c4598c49d00e020e5dead40228a84ed3eb
Brian Kildow <bkildow@gmail.com>
2014-11-24 07:47:55 -0500

    Stopped shipping apache access logs

--- log_client/manifests/web.pp
+++ log_client/manifests/web.pp
@@ -1,17 +1,7 @@
 class log_client::web inherits log_client {
 
-  logstashforwarder::file { 'apache-access':
-    paths  => [ '/var/log/httpd/access_log' ],
-    fields => { 'type' => 'apache-access' }
-  }
-
-  logstashforwarder::file { 'apache-access-ssl':
-    paths  => [ '/var/log/httpd/ssl_access_log' ],
-    fields => { 'type' => 'apache-access-ssl' }
-  }
-
   logstashforwarder::file { 'apache-error':
     paths  => [ '/var/log/httpd/error_log', '/var/log/httpd/ssl_error_log'],
     fields => { 'type' => 'apache-error' }
-  }	
+  }
 }
