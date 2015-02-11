a571847c177bb73dbffda31277be778a90cf497a
Brian Kildow <bkildow@gmail.com>
2015-02-11 11:39:34 -0500

    updated ssl cert locations

--- log_client/manifests/init.pp
+++ log_client/manifests/init.pp
@@ -1,29 +1,5 @@
 class log_client {
 
-  file { '/etc/logstashforwarder/ssl/public.cert':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///private/etc/httpd/conf/public.cert',
-  } 
-
-  file { '/etc/logstashforwarder/ssl/ca-chain.cert':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///private/etc/httpd/conf/ca-chain.cert',
-  } 
-
-  file { '/etc/logstashforwarder/ssl/private.key':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///private/etc/httpd/conf/private.key',
-  }
-
   file { "/var/log/logstash-forwarder":
     ensure => "directory"
   }
@@ -31,9 +7,9 @@ class log_client {
   class { 'logstashforwarder':
     manage_repo  => true,
     servers  => [ 'logstash.web.engineering.osu.edu:4545' ],
-    ssl_key  => '/etc/logstashforwarder/ssl/private.key',
-    ssl_ca   => '/etc/logstashforwarder/ssl/ca-chain.cert',
-    ssl_cert => '/etc/logstashforwarder/ssl/public.cert',
+    ssl_key  => '/etc/httpd/conf/private.key',
+    ssl_ca   => '/etc/httpd/conf/ca-chain.cert',
+    ssl_cert => '/etc/httpd/conf/public.cert',
     init_template => 'logstashforwarder/etc/init.d/logstashforwarder.RedHat.erb'
   }
 
